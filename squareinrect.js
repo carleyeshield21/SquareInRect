c89f4dfb7012d3ba6615dfe2c9546c14aa7ed56b
carleyeshield <you@example.com>
2022-05-12 17:24:47 +0800

    squareInRect

---
+++ squareinrect.js
@@ -0,0 +1,11 @@
+function sqInRect(lng, wdth){
+    let arr = []
+    if(lng === wdth) return null
+    while(lng > 0 && wdth > 0){
+      arr.push(lng > wdth ? wdth : lng)
+      lng > wdth ? lng -= wdth : wdth -= lng
+    }
+    // return arr
+    console.log(arr)
+  }
+  sqInRect(250,56)
